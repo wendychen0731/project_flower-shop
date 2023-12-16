e7838b5a3547d80367e1ad9c93e1291562fa5edd
wendychen0731 <sun609099@gmail.com>
2023-12-16 19:50:49 +0800

    導覽列修正

--- JavaScript/nav.js
+++ JavaScript/nav.js
@@ -15,19 +15,19 @@ $(function(){
     })
 
     $(window).resize(function(){
-        if($(window).width()>768){
+        if($(window).width()>750){
+            // document.getElementById("nav_list").checked = false;
             $(".nav_ul").css({
                 display: "flex",
                 flexDirection: "row",
                 justifyContent: "space-evenly",
                 paddingTop: 0,
             })
-        } else if ($(window).width() <= 768) {
+        } else if ($(window).width() <= 750) {
             if (document.getElementById("nav_list").checked = true)
             $(".nav_ul").css({
                 flexDirection: "column",
                 justifyContent: "flex-start",
-                paddingTop: 50,
             })
 
         }
