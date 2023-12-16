fdd5e3d810ebbb59d005ecfbca994e0eca3985df
wendychen0731 <sun609099@gmail.com>
2023-12-16 15:19:54 +0800

    rwd_導覽列調整

--- JavaScript/cart.js
+++ JavaScript/cart.js
@@ -3,19 +3,20 @@ function init(){
 	let btnPlus = document.querySelectorAll(".increase");
 
 	for (let i=0; i<btnMinus.length; i++){
+		// 取得單價物件及值
+		let unitPriceObj = document.querySelectorAll(".cart_unitprice");
+		let unitPrice = parseInt(unitPriceObj[i].value);
+
+		//取得價格合計的物件及值
+		let totalPriceObj = document.querySelectorAll(".cart_totalprice");
+		let totalPrice = parseInt(totalPriceObj[i].value)
+
+		totalPriceObj.value = unitPrice
 
 		btnMinus[i].onclick=function(){
 			// 取得數量物件及值			
 			let qtyObj = document.querySelectorAll(".btn_group>input");
 			let qty=parseInt(qtyObj[i].value);
-
-			// 取得單價物件及值
-			let unitPriceObj = document.querySelectorAll(".cart_unitprice");
-			let unitPrice = parseInt(unitPriceObj[i].value);
-
-			//取得價格合計的物件及值
-			let totalPriceObj = document.querySelectorAll(".cart_totalprice");
-			let totalPrice = parseInt(totalPriceObj[i].value);
 
 			if (qty>1){
 				qtyObj[i].value=qty-1;
@@ -29,14 +30,6 @@ function init(){
 			// 取得數量物件及值			
 			let qtyObj = document.querySelectorAll(".btn_group>input");
 			let qty=parseInt(qtyObj[i].value);
-
-			// 取得單價物件及值
-			let unitPriceObj = document.querySelectorAll(".cart_unitprice");
-			let unitPrice = parseInt(unitPriceObj[i].value);
-
-			//取得價格合計的物件及值
-			let totalPriceObj = document.querySelectorAll(".cart_totalprice");
-			let totalPrice = parseInt(totalPriceObj[i].value);
 
             if (qty<10){
                 qtyObj[i].value=qty+1;
--- JavaScript/nav.js
+++ JavaScript/nav.js
@@ -1,6 +1,9 @@
 window.addEventListener("scroll", function(){
     if (document.documentElement.scrollTop>800){
         document.getElementById("header").classList.add("smallPanel")
+        document.querySelector("ul.nav_ul").style.display="none";
+        document.getElementById("nav_list").checked = false;
+
     } else{
         document.getElementById("header").classList.remove("smallPanel")
     }
@@ -8,7 +11,6 @@ window.addEventListener("scroll", function(){
 
 $(function(){
     $("#navList").click(function(){
-        $(".nav_ul").fadeToggle(800);
+        $(".nav_ul").slideToggle(800);
     })
-
 })
