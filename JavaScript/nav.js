f4c4c26d1335c65f1aae6ad8305e3c054b06c344
wendychen0731 <sun609099@gmail.com>
2023-12-16 16:24:55 +0800

    導覽列問題修正

--- JavaScript/nav.js
+++ JavaScript/nav.js
@@ -13,4 +13,23 @@ $(function(){
     $("#navList").click(function(){
         $(".nav_ul").slideToggle(800);
     })
+
+    $(window).resize(function(){
+        if($(window).width()>768){
+            $(".nav_ul").css({
+                display: "flex",
+                flexDirection: "row",
+                justifyContent: "space-evenly",
+                paddingTop: 0,
+            })
+        } else if ($(window).width() <= 768) {
+            if (document.getElementById("nav_list").checked = true)
+            $(".nav_ul").css({
+                flexDirection: "column",
+                justifyContent: "flex-start",
+                paddingTop: 50,
+            })
+
+        }
+    })
 })
