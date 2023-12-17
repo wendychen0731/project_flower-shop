f7201de9481734663b5b207fcd91f9a70e46556c
wendychen0731 <sun609099@gmail.com>
2023-12-17 18:01:19 +0800

    細節修正

--- JavaScript/nav.js
+++ JavaScript/nav.js
@@ -1,8 +1,8 @@
 window.addEventListener("scroll", function(){
     if (document.documentElement.scrollTop>800){
         document.getElementById("header").classList.add("smallPanel")
-        document.querySelector("ul.nav_ul").style.display="none";
-        document.getElementById("nav_list").checked = false;
+        // document.querySelector("ul.nav_ul").style.display="none";
+        // document.getElementById("nav_list").checked = false;
 
     } else{
         document.getElementById("header").classList.remove("smallPanel")
@@ -16,7 +16,7 @@ $(function(){
 
     $(window).resize(function(){
         if($(window).width()>750){
-            // document.getElementById("nav_list").checked = false;
+            document.getElementById("nav_list").checked = false;
             $(".nav_ul").css({
                 display: "flex",
                 flexDirection: "row",
